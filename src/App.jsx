ca13cbf13087f098617bfb9972ec28a9d4027bb5
Sneha Pradeep <145252824+snehapradeep25@users.noreply.github.com>
2025-01-26 13:11:29 +0530

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Admin from './pages/home/AdminPanel/Admin'
